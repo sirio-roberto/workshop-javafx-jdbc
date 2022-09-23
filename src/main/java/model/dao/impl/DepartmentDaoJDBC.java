ed51743a789746a24d4312a8c7e9620841f63e50
Sirio <sirioroberto1@hotmail.com>
2022-09-22 23:34:22 -0300

    Remove department

--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -2,6 +2,7 @@ package model.dao.impl;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -72,7 +73,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             if (affectedRows == 0)
                 throw new DbException("there is no record with this Id");
         } catch (SQLException e) {
-            throw new DbException(e.getMessage());
+            throw new DbIntegrityException(e.getMessage());
         } finally {
             DB.closeStatement(st);
         }
